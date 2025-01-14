cced6e153e79182c8e1462b53dac0a5ffc924b62
abhin <abhin.pik@gmail.com>
2025-01-14 22:55:54 +0000

    refactor: enhance Socket.IO client event handling with constants and improved message structure

--- socketIO/iOClient.js
+++ socketIO/iOClient.js
@@ -1,23 +1,45 @@
 import httpServer from "./httpServer.js";
 import { Server } from "socket.io";
-
-const CONNECTION = "connection";
-const CONNECTED = "connected";
-const DISCONNECTED = "disconnect"
+import {
+  SEND,
+  CONNECTION,
+  CONNECTED_TO_IO_SERVER,
+  CONNECTED_TO_CHAT,
+  DISCONNECTED_FROM_SERVER,
+  USER_JOINED,
+} from "../utils/chatEvents.js";
 
 const iOClient = new Server(httpServer, {
   cors: { origin: process.env.CLIENT_HOST_URL },
 });
 
 iOClient.on(CONNECTION, (socket) => {
-  console.log("New IO connection established", socket.id);
-
-  socket.emit(CONNECTED, {
+  socket.emit(CONNECTED_TO_IO_SERVER, {
+    success: true,
     message: `You are connected to the server`,
     socket: socket.id,
   });
 
-  socket.on(DISCONNECTED, (payload) => {
+  socket.on(CONNECTED_TO_CHAT, (payload) => {
+    const { sender } = payload;
+    iOClient.emit(USER_JOINED, {
+      success: true,
+      userId: sender
+    });
+  });
+
+  socket.on("send-message", (payload) => {
+    const { chatMsg, roomNum, name } = payload;
+
+    iOClient.to(roomNum).emit("new-chat-message", {
+      message: chatMsg,
+      senderName: name,
+      senderSocketId: socket.id,
+      roomNum,
+    });
+  });
+
+  socket.on(DISCONNECTED_FROM_SERVER, (payload) => {
     console.log(`${socket.id} disconnected from server`, payload);
   });
 });
---
+++ utils/chatEvents.js
@@ -0,0 +1,6 @@
+export const SEND = "send";
+export const CONNECTION = "connection";
+export const CONNECTED_TO_IO_SERVER = "connected-to-io-server";
+export const DISCONNECTED_FROM_SERVER = "disconnect"
+export const USER_JOINED = "user-joined";
+export const CONNECTED_TO_CHAT = "connected-to-chat";
